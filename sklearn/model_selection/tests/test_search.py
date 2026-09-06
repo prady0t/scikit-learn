036c21b77da459110dccd45ee623f78944cc48ff
Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>
2026-09-06 19:22:33 +0530

    fix suggestions final
    Signed-off-by: Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>

--- sklearn/model_selection/tests/test_search.py
+++ sklearn/model_selection/tests/test_search.py
@@ -2034,12 +2034,12 @@ def test_custom_run_search():
             if not k.endswith("_time"):
                 # XXX: results['params'] is a list :|
                 results[k] = np.asanyarray(results[k])
-                actual = np.ma.getmaskarray(exp_results[k]).astype(int)
-                desired = np.ma.getmaskarray(results[k]).astype(int)
+                desired = np.ma.getmaskarray(exp_results[k]).astype(int)
+                actual = np.ma.getmaskarray(results[k]).astype(int)
                 if results[k].dtype.kind == "O":
-                    assert_array_equal(actual, desired, err_msg="Checking " + k)
+                    assert_array_equal(desired, actual, err_msg="Checking " + k)
                 else:
-                    assert_allclose(actual, desired, err_msg="Checking " + k)
+                    assert_allclose(desired, actual, err_msg="Checking " + k)
 
     def fit_grid(param_grid):
         return GridSearchCV(clf, param_grid, return_train_score=True).fit(X, y)
